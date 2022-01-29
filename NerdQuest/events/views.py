a471b3a846968df6c36583dcfabf52a0404f2462
David Allen <Doakungfu@gmail.com>
2022-01-29 15:18:23 -0500

    Log/Reg
    Initialized the paths and added the log/reg models and views to the events app.

--- NerdQuest/events/views.py
+++ NerdQuest/events/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+import bcrypt
 
-# Create your views here.
+def index(request):
+    return render(request, 'workouts/index.html')
+
+def register(request):
+    errors = User.objects.register_validator(request.POST)
+
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        user = User.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        )
+        request.session['user_id'] = user.id
+        request.session['greeting'] = user.first_name
+        return redirect('/workouts')
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        user = User.objects.get(email=request.POST['login_email'])
+        request.session['user_id'] = user.id
+        request.session['greeting'] = user.first_name
+        return redirect('/workouts')
+
+def logout(request):
+    request.session.flush()
+
+    return redirect('/')
