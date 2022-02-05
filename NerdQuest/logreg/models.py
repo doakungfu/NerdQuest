ff955d71a07508447fb3c37fae5092e3a662ba19
tootsie60 <73935697+tootsie60@users.noreply.github.com>
2022-02-05 05:06:47 -0800

    cleaned models

--- NerdQuest/logreg/models.py
+++ NerdQuest/logreg/models.py
@@ -35,6 +35,6 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
-from django.db import models
+ 
 
 # Create your models here.
