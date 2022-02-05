56579d16023e768b23e122826987f9eb54a39af5
tootsie60 <73935697+tootsie60@users.noreply.github.com>
2022-02-05 05:36:15 -0800

    urls cleaned up

--- NerdQuest/events/urls.py
+++ NerdQuest/events/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path("all", views.all_games),
+    path("/all", views.all_games),
     path("", views.games),
-    path("new", views.new_game),
-    path("add", views.add_game)
+    path("/new", views.new_game),
+    path("/add", views.add_game)
     # path("games/<int:game_id>", views.onegame), Kevin will do
     # path("games/<int:game_id>/edit>",views.edit ),David will do
     # path("games/<int:game_id/update", views.update),David will do
