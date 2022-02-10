9b469a26bde03cbd93379a1175f574a934c3f0ba
David Allen <Doakungfu@gmail.com>
2022-02-09 20:01:05 -0500

    werk

--- NerdQuest/events/urls.py
+++ NerdQuest/events/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('login', views.login),
     path('logout', views.logout),
     path('all', views.all_games),
-    path('games', views.my_games),
-    path('new', views.new_game),
-    path('add', views.add_game),
-    path('<int:game_id>', views.one_game),
-    path('<int:game_id>/edit',views.edit),
-    path('<int:game_id>/update', views.update),
-    path('<int:game_id>/delete', views.delete)
+    path('games/', views.my_games),
+    path('games/new', views.new_game),
+    path('games/add', views.add_game),
+    path('games/<int:game_id>', views.one_game),
+    path('games/<int:game_id>/edit',views.edit),
+    path('games/<int:game_id>/update', views.update),
+    path('games/<int:game_id>/delete', views.delete)
      
 ]
